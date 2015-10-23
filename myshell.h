49986ad07cfa4967cdd8b923fa37fa11b130e641
Calvin Lo <cheuk.lo@uoit.net>
2015-10-23 01:18:32 -0400

    Lab2 Final

--- myshell.h
+++ myshell.h
@@ -11,5 +11,21 @@
 // Include your relevant functions declarations here they must start with the 
 // extern keyword such as in the following example:
 // extern void display_help(void);
+extern int lshloop(void);
+extern char *lsh_read_line(void);
+extern char **lsh_split_line(char *line);
+extern int lsh_launch(char **args);
+
+extern int lsh_cd(char **args);
+extern int lsh_clr();
+extern int lsh_dir();
+extern int lsh_environ();
+extern int lsh_echo();
+extern int lsh_help();
+extern int lsh_pause();
+extern int lsh_quit();
+
+extern int lsh_num_builtins();
+extern int lsh_execute(char **args);
 
 #endif /* MYSHELL_H_ */
